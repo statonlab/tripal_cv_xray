859e6e207a6d23dedeb9174f7149f914748d0fc0
Bradford Condon <bradford.condon@gmail.com>
2018-04-26 16:22:23 -0400

    use factories to create features with related cvterms

--- tests/ChadoEntityChildCvtermTest.php
+++ tests/ChadoEntityChildCvtermTest.php
@@ -7,19 +7,11 @@ class entity_child_cvterm_test extends TripalTestCase {
 
   use DBTransaction;
 
-  static $obj;
-
-  protected function setUp() {
-
-    $this->obj = new ChadoEntityChildCvterm();
-
-  }
-
 
   public function test_get_all_associated_cvterms() {
-    $obj = $this->obj;
+    $obj = new ChadoEntityChildCvterm();
 
-//look up a random record ID with a prop
+    //look up a random record ID with a prop
     $query = db_select("chado.feature", 'F')
       ->fields('F', ['feature_id']);
     $query->join('chado.featureprop', 'FP', 'F.feature_id = FP.feature_id');
@@ -39,7 +31,7 @@ class entity_child_cvterm_test extends TripalTestCase {
 
   public function test_associate_entity_across_ancestors() {
 
-//look up a random record ID children in cvtermpath
+    //look up a random record ID children in cvtermpath
     $query = db_select("chado.featureprop", 'FP')
       ->fields('FP', ['type_id']);
 
@@ -53,12 +45,13 @@ class entity_child_cvterm_test extends TripalTestCase {
       return;
     }
 
-    $obj = $this->obj;
+    $obj = new ChadoEntityChildCvterm();
     $ancestors = $obj->fetch_ancestors($cvterm_id->type_id); //prop_type parameters are defined in get_all_associated_cvterms
 
     $this->assertNotEmpty($ancestors);
 
   }
+
 
   //WARNING: this test runs the update all entities job!
   //It's going to take a long time!  Run it to populate your entity term index...
@@ -66,29 +59,69 @@ class entity_child_cvterm_test extends TripalTestCase {
 
     //look up cvterm for mrna
 
-//    $query = db_select("chado.cvterm", "cvt")
-//      ->fields('cvt', ['cvterm_id'])
-//      ->condition('cvt.name', 'mRNA');
-//    $query->join('chado.cv', 'cv', 'cvt.cv_id cv.cv_id');
-//    $query->condition('cv.name', 'sequence');
-//    $mrna_id = $query ->execute()->fetchField();
-
-//    $organism = factory('chado.organism')->create();
-//    $features = factory('chado.feature', 100)->create([
-//      'organism_id' => $organism->id,
-//      'type_id' => $mrna_id,
-//    ]);
-//     $features =  factory('chado.feature', 100)->create();
+    $mrna_id = db_select("chado.cvterm", "CVT")
+      ->fields('CVT', ['cvterm_id'])
+      ->condition('CVT.name', 'mRNA')
+      ->execute()->fetchField();
 
 
-    $obj = $this->obj;
-    $obj->update_all_entities();
+    $organism = factory('chado.organism')->create();
+
+    $features = factory('chado.feature', 100)->create([
+      'organism_id' => $organism->organism_id,
+      'type_id' => $mrna_id,
+    ]);
+
+    $related_terms = $this->set_fake_cvterm_relationships();
+    //randomly assign these features some props with relationships
+
+    factory('chado.feature_cvterm')->create();
+
+    foreach ($features as $feature) {
+      $term = $related_terms[array_rand($related_terms)];
+      
+      factory('chado.feature_cvterm')->create([
+        'feature_id' => $feature->feature_id,
+        'cvterm_id' => $term->cvterm_id,
+      ]);
+    }
+
+    $obj = new ChadoEntityChildCvterm();
+    //   $obj->update_all_entities();
 
     $result = db_select("public.tripal_cvterm_entity_index", 't')
       ->fields('t')
       ->execute();
 
     $this->assertNotEquals($result->rowCount(), 0);
+  }
+
+  private function set_fake_cvterm_relationships() {
+    //Add some fake cvterms with relationships
+
+    $cv = factory("chado.cv")->create();
+
+    $cvterms = factory("chado.cvterm", 20)->create([
+      'cv_id' => $cv->cv_id,
+    ]);
+
+    $cvterms_to_pop = $cvterms;
+
+    $count = 0;
+    while ($count < 10) {
+      $subject = array_pop($cvterms_to_pop)->cvterm_id;
+      $object = array_pop($cvterms_to_pop)->cvterm_id;
+
+      $cvterm_relationships = factory('chado.cvterm_relationship')->create([
+        'subject_id' => $subject,
+        'object_id' => $object,
+      ]);
+
+      $count++;
+    }
+
+    return $cvterms;
+
   }
 
 }
