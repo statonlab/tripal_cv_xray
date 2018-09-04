38a53d002d64c4f98a64c94fa5c73a42190e84f9
bradford.condon <bradford.condon@gmail.com>
2018-09-04 10:46:13 -0400

    remove hook tests

--- tests/XrayIndexerJobTest.php
+++ tests/XrayIndexerJobTest.php
@@ -126,55 +126,6 @@ class XrayIndexerJobTest extends TripalTestCase {
   }
 
   /**
-   * The setUp method publishes annotated entities that trigger the entity
-   * insert hook. This test makes sure the hook got triggered and that
-   * the items appear in the index after the job gets executed.
-   *
-   * @throws \Exception
-   */
-  public function testInsertHookIsTriggered() {
-    $this->runQueuedJobs();
-
-    // Verify the entity exists in the index
-    $count = db_select('tripal_cvterm_entity_linker', 'TCEL')
-      ->condition('entity_id', $this->entity_id)
-      ->countQuery()
-      ->execute()
-      ->fetchField();
-    $this->assertGreaterThan(0, (int) $count);
-  }
-
-  /**
-   * Test that updating an entity updates the index.
-   *
-   * @throws \Exception
-   */
-  public function testUpdateHookIsTriggered() {
-    // Run the job to index the recently published entity
-    $this->runQueuedJobs();
-
-    // Delete the relationship from feature_cvterm and trigger an update
-    db_delete('chado.feature_cvterm')
-      ->condition('feature_id', $this->mrna->feature_id)
-      ->execute();
-    $ec = entity_get_controller('TripalEntity');
-    $entities = $ec->load([$this->entity_id]);
-    $entity = reset($entities);
-    module_invoke_all('entity_update', $entity, 'TripalEntity');
-
-    // Run the queued jobs
-    $this->runQueuedJobs();
-
-    // Verify the entity exists in the index
-    $count = db_select('tripal_cvterm_entity_linker', 'TCEL')
-      ->condition('entity_id', $this->entity_id)
-      ->countQuery()
-      ->execute()
-      ->fetchField();
-    $this->assertEquals(0, (int) $count);
-  }
-
-  /**
    * @ticket 39
    * @group failing
    * The indexer previously ended up with many identical entries: for example,
